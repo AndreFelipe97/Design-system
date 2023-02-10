0e7f891a3eb59ad63d104b16d7ace4b16f55cdd3
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2023-02-10 13:48:57 -0300

    feat: create multiStep component

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -8,30 +8,36 @@ export default {
 
   args: {
     children: 'Send',
+    variant: 'primary',
+    size: 'md',
+    disabled: false,
+  },
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'tertiary'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    size: {
+      options: ['sm', 'md'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    onClick: {
+      action: 'click',
+    },
   },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
 
-export const Secondary: StoryObj<ButtonProps> = {
-  args: {
-    children: 'Create New',
-    variant: 'secondary',
-  },
-}
-
-export const Tertiary: StoryObj<ButtonProps> = {
-  args: {
-    children: 'Cancel',
-    variant: 'tertiary',
-  },
-}
-
-export const Small: StoryObj<ButtonProps> = {
-  args: {
-    size: 'sm',
-  },
-}
 export const WithIcon: StoryObj<ButtonProps> = {
   args: {
     children: (
@@ -39,11 +45,5 @@ export const WithIcon: StoryObj<ButtonProps> = {
         Next step <ArrowRight weight="bold" />
       </>
     ),
-  },
-}
-
-export const Disabled: StoryObj<ButtonProps> = {
-  args: {
-    disabled: true,
   },
 }
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -6,8 +6,31 @@ export default {
   component: Text,
 
   args: {
+    size: 'md',
     children:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium doloremque quod fugit impedit odio soluta, quidem laboriosam tenetur.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
